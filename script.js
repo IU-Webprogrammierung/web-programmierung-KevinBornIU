7f53d50a0db4f3c4c2284d0d1773975e25b9624c
KevinBornIU <kevin.born@iubh-fernstudium.de>
2024-07-20 12:12:11 +0200

    Changed used Tags for Top-Up-Button to a real Button with animation through JS & CSS.

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+// Top-Up Button
+
+window.onload = function() {
+    const top_up = document.getElementById("top_up");
+
+// Zeigt Button, sobald gescrollt wird an der in CSS definierten Stelle.
+    window.onscroll = function() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            top_up.classList.add("show");
+            top_up.classList.remove("hide");
+        } else {
+            top_up.classList.add("hide");
+            top_up.classList.remove("show");
+        }
+    };
+
+// Scrollt nach oben, wenn Button geklickt wird.
+    top_up.onclick = function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+};
